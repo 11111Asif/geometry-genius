8794aff0640984a0b8a5b080659492ea7fa97aeb
11111Asif <asif040502@gmail.com>
2025-03-12 13:59:23 -0700

    added to rectangle

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -3,4 +3,12 @@ function rectangleCalculateArea(){
     const rectangleWidthText = rectangleWidthInput.value;
     const width = parseFloat(rectangleWidthText)
     console.log(width)
+
+    const rectangleLengthInput = document.getElementById('triangle-length')
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText)
+    console.log(length)
+
+    const area = width * length;
+    console.log('area of the rectangle is:', area)
 }
