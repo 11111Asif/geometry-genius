4757731a9fa7f9308241c77828863c212805e56b
11111Asif <asif040502@gmail.com>
2025-03-12 13:45:38 -0700

    added to triangle

---
+++ scripts/rectangle-calculate.js
@@ -0,0 +1,6 @@
+function rectangleCalculateArea(){
+    const rectangleWidthInput = document.getElementById('triangle-width')
+    const rectangleWidthText = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthText)
+    console.log(width)
+}
